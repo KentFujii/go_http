239f1aa0d04d6bb219f58e5d472edd2d7dd8d038
KentFujii <kent.where.the.light.is@gmail.com>
2018-08-25 21:58:01 +0900

    fix: restructure

--- http1.1/https.go
+++ http1.1/https.go
@@ -13,15 +13,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 		return
 	}
-
 	fmt.Println(string(dump))
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
 func main() {
-	var httpServer http.Server
 	http.HandleFunc("/", handler)
-	log.Printf("Start http listening :18888")
-	httpServer.Addr = ":18888"
-	log.Println(httpServer.ListenAndServe())
+	log.Println("start http listening :18443")
+	err := http.ListenAndServeTLS(":18443", "/certs/server.crt", "/certs/server.key", nil)
+	log.Println(err)
 }
