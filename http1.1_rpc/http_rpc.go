4c07ad246befee5808dcb57c78a1e53842ceed31
KentFujii <kent.where.the.light.is@gmail.com>
2018-09-09 15:46:19 +0900

    finish: http1.1

--- http1.1_rpc/http_rpc.go
+++ http1.1_rpc/http_rpc.go
@@ -11,7 +11,7 @@ import (
 type Calculator int
 
 // RPCで外部から呼ばれるメソッド
-func (c *Calculator) Multiply(args Args, result :int) {
+func (c *Calculator) Multiply(args Args, result *int) error {
 	log.Printf("Multiply called: %d, %d\n", args.A, args.B)
 	*result = args.A * args.B
 	return nil
@@ -23,4 +23,20 @@ type Args struct {
 }
 
 func main() {
+	calculator := new(Calculator)
+	server := rpc.NewServer()
+	server.Register(calculator)
+	http.Handle(rpc.DefaultRPCPath, server)
+	log.Println("start http listening :18885")
+	listener, err := net.Listen("tcp", ":18885")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go server.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
 }
