ffc21a248da04939a916d69881dbd172c9b6410f
KentFujii <kent.where.the.light.is@gmail.com>
2019-04-27 00:29:03 +0900

    fix

--- http1.1_server_certs/build.sh
+++ http1.1_server_certs/build.sh
@@ -3,7 +3,7 @@ dep init || dep ensure
   for f in $(ls client/ | sed 's/\.go//')
   do
     echo $f
-    go build -o bin/$f client/$f.go
+    GOOS=darwin GOARCH=amd64 go build -o bin/$f client/$f.go
   done
 )
 go run server.go
